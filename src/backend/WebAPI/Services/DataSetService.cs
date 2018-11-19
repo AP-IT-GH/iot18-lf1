0bd8b735013c2c82b420408a190454c076474289
Unknown <s096257@ap.be>
2018-11-19 14:04:51 +0100

    Add herb datasets
    Added repository,service,model and controller for herb datasets. These datasets contain setpoints for sensors. (datasets for top 10 herbs)

---
+++ src/backend/WebAPI/Services/DataSetService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+using Repositories;
+
+namespace Services
+{
+    public class DataSetService
+    {
+        private DataSetRepository _dataSetRepository;
+
+        public DataSetService(DataSetRepository dataSetRepository)
+        {
+            _dataSetRepository = dataSetRepository;
+        }
+
+        public PlantDataSet Create(PlantDataSet picture)
+        {
+            return _dataSetRepository.Post(picture);
+        }
+
+        public PlantDataSet Update(PlantDataSet picture)
+        {
+            return _dataSetRepository.Put(picture);
+        }
+
+        public bool Delete(int id)
+        {
+            return _dataSetRepository.Delete(id);
+        }
+
+        public List<PlantDataSet> GetAll()
+        {
+            return _dataSetRepository.GetAll();
+        }
+
+        public PlantDataSet Get(int id)
+        {
+            return _dataSetRepository.Get(id);
+        }
+
+        public List<string> GetPlants()
+        {
+            var names = new List<string>();
+            var dataSets = GetAll();
+            foreach(PlantDataSet p in dataSets)
+            {
+                names.Add(p.PlantSpecies);
+            }
+            return names;
+        }
+    }
+}
---
+++ src/backend/WebAPI/WebAPI/Controllers/DataSetController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models;
+using Services;
+
+namespace WebAPI.Controllers
+{
+    //API endpoints
+    [Route("api/v1/dataset")]
+    public class DataSetController : Controller
+    {
+        private DataSetService _dataSetService;
+
+        public DataSetController(DataSetService dataSetService)
+        {
+            _dataSetService = dataSetService;
+        }
+
+        [HttpGet]
+        public IEnumerable<PlantDataSet> Get()
+        {
+            return _dataSetService.GetAll();
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public PlantDataSet Get(int id)
+        {
+            return _dataSetService.Get(id);
+        }
+
+        [Route("names")]
+        [HttpGet]
+        public List<string> GetName()
+        {
+            return _dataSetService.GetPlants();
+        }
+
+        [HttpPut("{id}")]
+        public PlantDataSet Put([FromBody]PlantDataSet dataSet)
+        {
+            return _dataSetService.Update(dataSet);
+        }
+
+        [HttpPost]
+        public PlantDataSet Post([FromBody]PlantDataSet dataSet)
+        {
+            return _dataSetService.Create(dataSet);
+        }
+
+
+        [Route("{id}")]
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            return _dataSetService.Delete(id);
+        }
+
+
+    }
+
+}
