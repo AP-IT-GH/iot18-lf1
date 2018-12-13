ea8e94a3792e54b98bc0e92f21943d7300144efa
Melvin Bootsgezel <m.bootsgezel@live.nl>
2018-12-13 21:15:54 +0100

    Notification controller added

--- src/backend/WebAPI/WebAPI/Controllers/NotificationController.cs
+++ src/backend/WebAPI/WebAPI/Controllers/NotificationController.cs
@@ -1,7 +1,41 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+using Services;
+using Models;
+
+
 namespace WebAPI.Controllers {
+
     public class NotificationController {
-        public NotificationController() {
+
+        private NotificationService _service;
+
+
+        public NotificationController(NotificationService service) {
+            _service = service;
         }
+
+        [HttpGet]
+        public List<Notification> GetList() {
+            return _service.GetAll();
+        }
+
+        [HttpGet("{authid}")]
+        public List<Notification> GetAll(string authid) {
+            return _service.GetAll(authid);
+        }
+
+        [HttpPost]
+        public Notification Post([FromBody]Notification notification) {
+            return _service.Post(notification);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id) {
+            return _service.Delete(id);
+        }
+
     }
+
 }
