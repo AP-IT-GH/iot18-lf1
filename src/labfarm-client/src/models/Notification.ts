3964764c2c983d0c94453907843a88835e6267d1
Melvin Bootsgezel <m.bootsgezel@live.nl>
2018-12-13 21:08:29 +0100

    Notifications: backend api communication added

--- src/labfarm-client/src/models/Notification.ts
+++ src/labfarm-client/src/models/Notification.ts
@@ -2,6 +2,7 @@ import { NotificationUrgency } from "./NotificationUrgency";
 
 export interface Notification {
     id: number,
+    authId: string,
     title: string,
     body: string,
     linkToUrl: string,
--- src/labfarm-client/src/providers/notifications/notifications.service.ts
+++ src/labfarm-client/src/providers/notifications/notifications.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { NotificationUrgency } from 'src/models/NotificationUrgency';
 import { Notification } from 'src/models/Notification';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
     providedIn: 'root'
@@ -10,14 +13,14 @@ export class NotificationsService {
     private notifications: Notification[] = [];
 
 
-
-    constructor() {
+    constructor(private http: HttpClient, private auth: AuthenticationService) {
         this.initDemoNotifications();
     }
 
     public initDemoNotifications() {
         this.notifications.push({
             id: 1,
+            authId: "admin",
             title: "Notification 1 header",
             body: "Notification with low urgency",
             linkToUrl: "/home",
@@ -26,6 +29,7 @@ export class NotificationsService {
 
         this.notifications.push({
             id: 2,
+            authId: "admin",
             title: "Notification 2 header",
             body: "Notification with normal urgency",
             // linkToUrl: "/home",
@@ -35,6 +39,7 @@ export class NotificationsService {
 
         this.notifications.push({
             id: 3,
+            authId: "admin",
             title: "Notification 3 header",
             body: "Notification with high urgency",
             linkToUrl: "/home",
@@ -42,6 +47,7 @@ export class NotificationsService {
         });
         this.notifications.push({
             id: 4,
+            authId: "admin",
             title: "Notification 4 header",
             body: "Notification with extreme urgency",
             //   linkToUrl: "/home",
@@ -50,6 +56,7 @@ export class NotificationsService {
         });
         // this.notifications.push({
         //     id: 5,
+        //     authId: "admin",
         //     title: "Notification 5 header",
         //     body: "Notification with normal urgency",
         //     linkToUrl: "/home",
@@ -59,6 +66,7 @@ export class NotificationsService {
 
         // this.notifications.push({
         //     id: 6,
+        //     authId: "admin",
         //     title: "Notification 6 header",
         //     body: "Notification with normal urgency",
         //     // linkToUrl: "/home",
@@ -67,8 +75,24 @@ export class NotificationsService {
         // });
     }
 
+    public initNotifications() {
+
+    }
+
     public getNotifications(): Notification[] {
         return this.notifications;
+    }
+
+    public getRealNotifications(): Observable<Notification[]> {
+        console.log("Getting notifications for user " + this.auth.getAuthId());
+
+        return this.http.get<Notification[]>(`/notification/${this.auth.getAuthId()}`);
+    }
+
+    public deleteNotification(id: number): Observable<boolean>{
+        console.log("Deleting notification with ID: " + id);
+
+        return this.http.delete<boolean>(`/notification/${id}`);
     }
 
     public remove(notification: Notification): boolean {
